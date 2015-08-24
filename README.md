6c8bc7b5cd694657f3b98d9e3958cc46e5c71946
Claudius Coenen <opensource@amenthes.de>
2015-08-24 11:48:02 +0200

    adding options and passing them on to transform

--- README.md
+++ README.md
@@ -18,3 +18,16 @@ var outputTree = react(inputTrees);
 
 * **`inputTrees`**: An array of trees that act as the include paths for
   react jsx files. If you have a single tree, pass `[tree]`.
+
+## options
+
+```
+react(inputTrees, {
+  encoding: 'something', // encoding of the jsx files (defaults to UTF-8)
+  transformOptions: {    // These are passed to `transform`
+    sourceMap: true
+  }
+});
+```
+
+For all possible `transformOptions`, [see transform in react-tools](https://www.npmjs.com/package/react-tools#transform-inputstring-options).
--- index.js
+++ index.js
@@ -6,32 +6,34 @@ var CachingWriter = require('broccoli-caching-writer');
 var glob = require('glob');
 var react = require('react-tools').transform;
 
-module.exports = ReactCompiler;
-ReactCompiler.prototype = Object.create(CachingWriter.prototype);
-ReactCompiler.prototype.constructor = ReactCompiler;
-function ReactCompiler (inputTrees) {
-  if (!(this instanceof ReactCompiler)) return new ReactCompiler(inputTrees);
+module.exports = ReactCaching;
+ReactCaching.prototype = Object.create(CachingWriter.prototype);
+ReactCaching.prototype.constructor = ReactCaching;
+function ReactCaching (inputTrees, options) {
+  if (!(this instanceof ReactCaching)) return new ReactCaching(inputTrees, options);
   if (!Array.isArray(inputTrees)) throw new Error('Expected array for first argument - did you mean [tree] instead of tree?');
 
-  CachingWriter.call(this, inputTrees, {filterFromCache: {include: [/\.jsx$/]}});
+  this.options = options || {};
+  this.options.encoding = this.options.encoding || 'UTF-8';
+  this.options.transformOptions = this.options.transformOptions || {};
 
-  this.inputTrees = inputTrees;
-  // this.options = options || {};
+  CachingWriter.call(this, inputTrees, {filterFromCache: {include: [/\.jsx$/]}});
 }
 
 
-ReactCompiler.prototype.updateCache = function(includePaths, destDir) {
+ReactCaching.prototype.updateCache = function(includePaths, destDir) {
+  reactCaching = this;
   includePaths.forEach(function (includePath) {
     var inFiles = glob.sync(path.join(includePath, '/**/*.jsx'), { nodir: true });
     inFiles.forEach(function (inFile) {
       debug("Processing ", inFile);
-      var content = fs.readFileSync(inFile, {encoding: 'UTF-8'});
-      var compiled = react(content);
+      var content = fs.readFileSync(inFile, {encoding: reactCaching.options.encoding});
+      var compiled = react(content, reactCaching.options.transformOptions);
       var relativePath = path.relative(includePath, inFile);
       var destFile = path.join(destDir, relativePath).replace(/jsx$/i, 'js');
       debug("output to: %s", destFile);
       mkdirp.sync(path.dirname(destFile));
-      fs.writeFileSync(destFile, compiled, {encoding: 'UTF-8'});
+      fs.writeFileSync(destFile, compiled, {encoding: reactCaching.options.encoding});
     });
   });
 };
